fe00ac63384b4bf287602b8b459c0ae0ad3ea203
Ryan White <ryanwhite04@gmail.com>
2021-05-13 16:32:26 +0800

    Show live camera filter

---
+++ project/livefeed.py
@@ -0,0 +1,25 @@
+from requests import get
+from filter import *
+def getImage(ip, path="camera.jpg"):
+    content = get(f'http://{ip}:4242/current.jpg?annotations=off').content
+    array = np.asarray(bytearray(content), dtype=np.uint8)
+    return cv.imdecode(array, -1)
+
+def showCamera(ip):
+    path = getImage(ip)
+    image = cv.imread(path)
+    filtered = filterImage(image, RED)
+    cv.imshow('camera', image)
+    cv.imshow('red', filtered)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+def liveFeed(ip, color=GREEN):
+    while True:
+        try:
+            result, mask = filterImage(getImage(ip), color)
+            cv.imshow(f'live {color}', result)
+        except Exception as e:
+            print(e)
+            break
+
