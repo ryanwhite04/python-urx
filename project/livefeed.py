a65761b3aa7d2ae3f7ee427619e52af939552de3
Ryan White <ryanwhite04@gmail.com>
2021-05-13 17:08:50 +0800

    Test out nonlive

--- project/livefeed.py
+++ project/livefeed.py
@@ -1,15 +1,19 @@
 from requests import get
 from filter import *
 
-def getCoordinates(ip, color):
-    result, mask = filterImage(cv.imread('clustered.jpg'), GREEN)
+def addCircles(result, mask):
     circles = cv.HoughCircles(mask, cv.HOUGH_GRADIENT, 2.5, 100, 200)
+    circles = np.uint16(np.around(circles))
+    for c in circles[0,:]:
+        cv.circle(result, (c[0], c[1]), 10, (255, 0, 0), 3)
+    return result
     
 
 def getImage(ip, path="camera.jpg"):
     content = get(f'http://{ip}:4242/current.jpg?annotations=off').content
     array = np.asarray(bytearray(content), dtype=np.uint8)
     return cv.imdecode(array, -1)
+
 
 def showCamera(ip):
     path = getImage(ip)
@@ -21,15 +25,17 @@ def showCamera(ip):
     cv.destroyAllWindows()
 
 def liveFeed(ip, color=GREEN):
-    while True:
-        try:
-            result, mask = filterImage(getImage(ip), color)
-            circles = cv.HoughCircles(mask, cv.HOUGH_GRADIENT, 2.5, 100, 200)
-            for c in circles[0]:
-                cv.circle(result, (c[0], c[1]), 10, (255, 0, 0), 3)
-            cv.imshow(f'live {color}', result)
-            cv.waitKey(1)
-        except Exception as e:
-            print(e)
-            break
+
+    result, mask = filterImage(getImage(ip), color)
+    cv.imshow(f'live {color}', addCircles(result, mask))
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    # while True:
+    #     try:
+    #         result, mask = filterImage(getImage(ip), color)
+    #         cv.imshow(f'live {color}', addCircles(result, mask))
+    #         cv.waitKey(1)
+    #     except Exception as e:
+    #         print(e)
+    #         break
 
